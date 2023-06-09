74f28dbe7e1e7523128386d6018ab6f72353c712
Isuru Bandara <89684055+razorisuru@users.noreply.github.com>
2023-06-09 23:38:25 +0530

    UI Changes

--- admin/header.php
+++ admin/header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link href="../css/style.css?v=<?php echo time(); ?>" rel="stylesheet" type="text/css">
+	<link href="../../css/view.css?v=<?php echo time(); ?>" rel="stylesheet" type="text/css">
 
 
     <!-- Bootstrap CSS -->
@@ -15,6 +15,8 @@
 
 <!-- Bootstrap Icon -->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" media="screen" />
+
     <title>ADMIN DASHBOARD</title>
 </head>
 <body>
--- admin/includes/home.php
+++ admin/includes/home.php
@@ -2,34 +2,85 @@
 <!-- Header -->
 <?php include "../header.php"?>
 
-<body style="background: url(../../img/bg.jpg);
-	background-size: cover;
-	background-position: center;
-	background-repeat: no-repeat;">
+<body class="main-container">
 
 
     <?php
   if ($_SESSION['role'] == 'super_admin'){
 ?>
-    <div class="container">
-        <h1 class="text-center">ADMIN DASHBOARD</h1>
-        <div class="row ">
-            <div class="col-md-4"></div>
-            <div class="col-md-8">
-                <a href="../../register.php" class='btn btn-outline-dark '> <i class="bi bi-person-plus"></i> Register A
-                    New User</a>
-                <a href="../../index.php" class="btn btn-warning "><i class="bi bi-house-door-fill"></i> HOME </a>
-                <a class="btn btn-danger" href='../../logout.php'><i class="bi bi-box-arrow-left"></i> Logout</a>
+
+    <!-- navbar -->
+    <nav class="navbar sticky-top navbar-expand-lg navc">
+        <div class="container-fluid">
+            <a class="navbar-brand text-light" href="../../view.php">
+                <img src="../../img/logo2.png" alt="Logo" width="40" class="d-inline-block align-text-top">
+                Niwahana
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText1"
+                aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon">
+                    <i class="fa-solid fa-bars" style="color:#fff; font-size:28px;"></i>
+                    <!-- <i class="bi bi-list" style="color:#fff; font-size:28px;"></i> -->
+                </span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarText1">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+
+                    <li class="nav-item">
+                        <a class="nav-link text-light" href="../../register.php">Emotional Damage</a>
+                    </li>
+                </ul>
+                <div class="dropdown">
+                    <button class="btn btnprofile dropdown-toggle" type="button" id="dropdownMenuButton1"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        Welcome <?php echo $_SESSION['name'] ?>
+                    </button>
+                    <ul class="btnprofiledropdown dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                        <li><a class="dropdown-item" data-bs-toggle="modal" data-bs-target="#profile" href="#">Profile
+                                <i class="fa-solid fa-user"></i></a></li>
+                        <li><a class="dropdown-item" href="../../logout.php"><span
+                                    class="btnprofiledropdownlogout">Logout <i
+                                        class="fa-solid fa-arrow-right-from-bracket"></i></span> </a></li>
+                    </ul>
+                </div>
             </div>
-
-
-
         </div>
+    </nav>
+
+    <!-- Modal -->
+    <div class="modal fade" id="profile" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content rounded-2 modi">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel"><?php echo $_SESSION['name'] ?>'s Profile <i
+                            class="fa-solid fa-user"></i></h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <img src="../../img/logo.png" class="img-thumbnail float-end" width="80" alt="...">
+                    Full Name : <?php echo $_SESSION['name'] ?><br>
+                    E-Mail : <?php echo $_SESSION['email'] ?><br>
+                    Role : <?php echo $_SESSION['role'] ?><br><br>
+                    <div class="text-light rounded-1 modalcost" id="sqftmodal"></div>
+                    <div class="text-light rounded-1 modalcost" id="modalcost"></div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btnclose float-start">Print</button>
+                    <button type="button" class="btn btnclose" data-bs-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
-        <table class="table table-primary rounded-4 mt-5 table-hover tablebg">
+
+
+
+    <div class="container">
+        <table class="table rounded-4 mt-5 table-hover ">
             <thead>
-                <tr>
+                <tr class="tablebg1">
                     <th scope="col">ID</th>
                     <th scope="col">Name</th>
                     <th scope="col">Username</th>
@@ -38,7 +89,7 @@
                 </tr>
             </thead>
             <tbody>
-                <tr>
+                <tr styl>
 
                     <?php
             $query="SELECT * FROM login";               // SQL query to fetch all table data
@@ -51,15 +102,15 @@
               $username = $row['username'];         
               $role = $row['role'];        
 
-              echo "<tr class='table-warning'>";
+              echo "<tr class='tablebg2'>";
               echo " <th scope='row' >{$id}</th>";
               echo " <td > {$name}</td>";
               echo " <td > {$username}</td>";
               echo " <td >{$role} </td>";
 
-              echo " <td class=''> <a href='view.php?user_id={$id}' class='btn btn-sm btn-outline-success'> <i class='bi bi-eye'></i> </a> </td>";
+              echo " <td class=''> <a href='view.php?user_id={$id}' class='btn btn-sm btn-outline-light'> <i class='bi bi-eye'></i> </a> </td>";
 
-              echo " <td class='' > <a href='update.php?edit&user_id={$id}' class='btn btn-sm btn-outline-secondary'><i class='bi bi-pencil'></i> </a> </td>";
+              echo " <td class='' > <a href='update.php?edit&user_id={$id}' class='btn btn-sm btn-outline-light'><i class='bi bi-pencil'></i> </a> </td>";
 
               echo " <td  class=''>  <a href='delete.php?delete={$id}'\" onClick=\"return confirm('Are you sure you want to delete?')\" class='btn btn-sm btn-outline-danger'> <i class='bi bi-trash'></i> </a> </td>";
               echo " </tr> ";
@@ -69,9 +120,9 @@
             </tbody>
         </table>
 
-        <table class="table table-primary rounded-4 mt-5 table-hover tablebg">
+        <table class="table rounded-4 mt-5 rounded-2 table-hover">
             <thead>
-                <tr>
+                <tr class="tablebg1">
                     <th scope="col">ID</th>
                     <th scope="col">Name</th>
                     <th scope="col">Email</th>
@@ -97,10 +148,10 @@
               $Length1 = $row['Length1'];        
               $Width1 = $row['Width1'];        
 
-              echo "<tr class='table-warning'>";
+              echo "<tr class='tablebg2'>";
               echo " <th scope='row' >{$id}</th>";
               echo " <td > {$name}</td>";
-              echo " <td > {$email}</td>";
+              echo " <td > <a href = 'mailto:{$email}'>{$email}</a></td>";
               echo " <td >{$Quantity1} </td>";
               echo " <td >{$Length1} </td>";
               echo " <td >{$Width1} </td>";
@@ -117,19 +168,20 @@
             </tbody>
         </table>
     </div>
+    </div>
 
 
     <!-- a BACK button to go to the index page -->
-    <div class="container text-center mt-5">
 
-        <div>
-
-            <?php 
+    <?php 
     }else {
       header('Location: ../../view.php');
     }
   ?>
 
-            <!-- Footer -->
-            <?php include "../footer.php" ?>
+    <!-- Footer -->
+    <?php include "../footer.php" ?>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
 </body>
