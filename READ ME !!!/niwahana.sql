6734d1716fdf77eee289f0c8d7c99af332e26d3c
Isuru Bandara <89684055+razorisuru@users.noreply.github.com>
2023-06-12 21:21:15 +0530

    Update niwahana.sql

--- READ ME !!!/niwahana.sql
+++ READ ME !!!/niwahana.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 09, 2023 at 07:19 PM
+-- Generation Time: Jun 12, 2023 at 05:50 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -24,55 +24,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `details`
---
-
-CREATE TABLE `details` (
-  `id` int(10) NOT NULL,
-  `Name` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `LivingRooms` int(255) NOT NULL,
-  `LivingRoomSqFt` int(255) NOT NULL,
-  `Bedrooms` int(255) NOT NULL,
-  `BedroomsSqFt` int(255) NOT NULL,
-  `DiningRooms` int(255) NOT NULL,
-  `DiningRoomSqft` int(255) NOT NULL,
-  `BathRooms` int(255) NOT NULL,
-  `BathRoomSqFt` int(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `details`
---
-
-INSERT INTO `details` (`id`, `Name`, `email`, `LivingRooms`, `LivingRoomSqFt`, `Bedrooms`, `BedroomsSqFt`, `DiningRooms`, `DiningRoomSqft`, `BathRooms`, `BathRoomSqFt`) VALUES
-(11, '$client', '$clientmail', 1, 2, 0, 0, 0, 0, 0, 0);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `details2`
---
-
-CREATE TABLE `details2` (
-  `id` int(10) NOT NULL,
-  `Name` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `Quantity1` int(255) NOT NULL,
-  `Length1` int(255) NOT NULL,
-  `Width1` int(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `details2`
---
-
-INSERT INTO `details2` (`id`, `Name`, `email`, `Quantity1`, `Length1`, `Width1`) VALUES
-(4, 'Amindu Sangeeth', 'amindusangeeth@gmail.com', 232, 232, 32);
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `login`
 --
 
@@ -82,34 +33,24 @@ CREATE TABLE `login` (
   `email` varchar(100) NOT NULL,
   `role` varchar(255) NOT NULL,
   `username` varchar(100) NOT NULL,
-  `password` varchar(100) NOT NULL
+  `password` varchar(100) NOT NULL,
+  `code` varchar(10) NOT NULL,
+  `update_time` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `login`
 --
 
-INSERT INTO `login` (`id`, `name`, `email`, `role`, `username`, `password`) VALUES
-(1, 'Isuru Bandara', 'isurubandara@gmail.com', 'super_admin', 'isuru', '827ccb0eea8a706c4c34a16891f84e7b'),
-(2, 'Idunil Bandara', 'idunilbandara@gmail.com', 'admin', 'idunil', '81dc9bdb52d04dc20036dbd8313ed055'),
-(3, 'Amindu Sangeeth', 'amindusangeeth@gmail.com', 'user', 'ami', '81dc9bdb52d04dc20036dbd8313ed055'),
-(4, 'Isuri Samaranayaka', 'isuri@gmail.com', 'user', 'isuri', '81dc9bdb52d04dc20036dbd8313ed055');
+INSERT INTO `login` (`id`, `name`, `email`, `role`, `username`, `password`, `code`, `update_time`) VALUES
+(1, 'Isuru Bandara', 'isurubandara318@gmail.com', 'super_admin', 'isuru', '827ccb0eea8a706c4c34a16891f84e7b', 'QIYPR54XMK', '2023-06-12 15:45:47'),
+(2, 'Idunil Bandara', 'idunilbandara@gmail.com', 'admin', 'idunil', '81dc9bdb52d04dc20036dbd8313ed055', '', '2023-06-12 15:14:01'),
+(3, 'Amindu Sangeeth', 'amindusangeeth@gmail.com', 'user', 'ami', '81dc9bdb52d04dc20036dbd8313ed055', '', '2023-06-12 15:14:01'),
+(16, 'Isuri Samaranayaka', 'isuri@gmail.com', '', 'isuri', '81dc9bdb52d04dc20036dbd8313ed055', '', '2023-06-12 15:14:01');
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `details`
---
-ALTER TABLE `details`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `details2`
---
-ALTER TABLE `details2`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `login`
@@ -122,22 +63,10 @@ ALTER TABLE `login`
 --
 
 --
--- AUTO_INCREMENT for table `details`
---
-ALTER TABLE `details`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
-
---
--- AUTO_INCREMENT for table `details2`
---
-ALTER TABLE `details2`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
 -- AUTO_INCREMENT for table `login`
 --
 ALTER TABLE `login`
-  MODIFY `id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
