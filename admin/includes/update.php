799871477cb0b449333e804314bcb0d51506ed8a
Isuru Bandara <89684055+razorisuru@users.noreply.github.com>
2023-06-19 19:31:05 +0530

    Update update.php

--- admin/includes/update.php
+++ admin/includes/update.php
@@ -62,12 +62,7 @@
             <label for="pass">Role</label>
             <input type="text" name="role" class="form-control" value="<?php echo $role  ?>">
         </div>
-        <select class="form-select" aria-label="Default select example">
-            <option selected>Open this select menu</option>
-            <option value="1">One</option>
-            <option value="2">Two</option>
-            <option value="3">Three</option>
-        </select>
+        
 
         <div class="form-group">
             <input type="submit" name="update" class="btn btn-primary mt-2" value="update">
